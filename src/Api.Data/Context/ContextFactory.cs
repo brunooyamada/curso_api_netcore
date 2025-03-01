daa16a13d5e5b9fbb5e04d613ba3ad7c2fc7133b
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-02-28 23:05:38 -0300

    57. Api.Data - Criar ContextFactory

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            // Usado para criar as migrações
+            var connectionString = "Server=localhost;Port=5432;Database=dbApi;Uid=postgres;Pwd=masterkey";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
