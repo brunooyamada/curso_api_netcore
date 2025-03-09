8470a1536e4c355fd5bd1dfcf506d979dff2391d
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-03-08 22:15:03 -0300

    66. Api.Domain - Implementar Interface UserService

---
+++ src/Api.Domain/Interfaces/Services/User/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Services.User
+{
+    public interface IUserService
+    {
+        Task<UserEntity> Get(long id);
+        Task<IEnumerable<UserEntity>> GetAll();
+        Task<UserEntity> Post(UserEntity user);
+        Task<UserEntity> Put(UserEntity user);
+        Task<bool> Delete(long id);
+    }
+}
