80055792e6016d85fe7420e17341cad98f75ca3b
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-03-16 23:19:15 -0300

    76. Api.Application - Implementar Swagger

--- src/Api.Application/Program.cs
+++ src/Api.Application/Program.cs
@@ -9,7 +9,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => 
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Curso de API com AspNetCore 8 - Na prática",
+        Description = "Arquitetura DDD",
+        TermsOfService = new Uri("http://www.mfrinfo.com.br"),
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Marcos Fabricio Rosa",
+            Email = "mfrinfo@mail.com",
+            Url = new Uri("http://www.mfrinfo.com.br"),
+        },
+        License = new Microsoft.OpenApi.Models.OpenApiLicense
+        {
+            Name = "Termo de licença de Uso",
+            Url = new Uri("http://www.mfrinfo.com.br"),
+        }
+    }
+));
 ConfigureService.ConfigureDependenciesService(builder.Services);
 ConfigureRepository.ConfigureDependenciesRepository(builder.Services);
 
