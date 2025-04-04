db58281592abcd0d1cd6acbedc54a3e185dbc259
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-04-04 17:41:41 -0300

    109. Implementar Classe Base Teste

---
+++ src/Api.Data.Test/BaseTest.cs
@@ -0,0 +1,42 @@
+﻿using Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Data.Test;
+
+public abstract class BaseTest
+{
+    protected BaseTest()
+    {
+        
+    }
+}
+
+public class DbTeste : IDisposable
+{
+    private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+    public ServiceProvider ServiceProvider { get; private set; }
+
+    public DbTeste()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddDbContext<MyContext>(o =>
+            o.UseNpgsql($"Server=localhost;Port=5432;Database={dataBaseName};Uid=postgres;Pwd=masterkey"), 
+                ServiceLifetime.Transient
+        );
+
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+        using (var context = ServiceProvider.GetService<MyContext>())
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+
+    public void Dispose()
+    {
+        using (var context = ServiceProvider.GetService<MyContext>())
+        {
+            context.Database.EnsureDeleted();
+        }
+    }
+}
