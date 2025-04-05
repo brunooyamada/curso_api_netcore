01db57e9fa69a9a0bbf47a8b1dfe23fd5b8f8d5f
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-04-04 23:59:01 -0300

    110. Implementar Teste de Inclusão de Usuário

---
+++ src/Api.Data.Test/UsuarioCrudCompleto.cs
@@ -0,0 +1,38 @@
+﻿using Data.Context;
+using Data.Implementations;
+using Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Data.Test
+{
+    public class UsuarioCrudCompleto : BaseTest, IClassFixture<DbTeste>
+    {
+        private ServiceProvider _serviceProvider;
+
+        public UsuarioCrudCompleto(DbTeste dbTeste)
+        {
+            _serviceProvider = dbTeste.ServiceProvider;
+        }
+
+        [Fact(DisplayName = "CRUD de Usuário")]
+        [Trait("CRUD", "UserEntity")]
+        public async Task E_Possivel_Realizar_CRUD_Usuario()
+        {
+            using (var context = _serviceProvider.GetService<MyContext>())
+            {
+                UserImplementation _repositorio = new UserImplementation(context);
+                UserEntity _entity = new UserEntity
+                {
+                    Email = "teste@mail.com",
+                    Name = "teste",
+                };
+
+                var _registroCriado = await _repositorio.InsertAsync(_entity);
+                Assert.NotNull(_registroCriado);
+                Assert.Equal("teste@mail.com", _registroCriado.Email);
+                Assert.Equal("teste", _registroCriado.Name);
+                Assert.False(_registroCriado.Id == 0);
+            }
+        }
+    }
+}
