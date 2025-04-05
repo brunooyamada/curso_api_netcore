88c128920f3ae07cb51220706529742ef4f901b1
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-04-05 00:30:38 -0300

    112. Finalizar os testes de CRUD de Usuário

--- src/Api.Data.Test/UsuarioCrudCompleto.cs
+++ src/Api.Data.Test/UsuarioCrudCompleto.cs
@@ -32,6 +32,37 @@ namespace Api.Data.Test
                 Assert.Equal(_entity.Email, _registroCriado.Email);
                 Assert.Equal(_entity.Name, _registroCriado.Name);
                 Assert.False(_registroCriado.Id == 0);
+
+                // update
+                _entity.Name = Faker.Name.First();
+                var _registroAtualizado = await _repositorio.UpdateAsync(_entity);
+                Assert.NotNull(_registroAtualizado);
+                Assert.Equal(_entity.Email, _registroAtualizado.Email);
+                Assert.Equal(_entity.Name, _registroAtualizado.Name);
+
+                // existe
+                var _registroExiste = await _repositorio.ExistAsync(_registroAtualizado.Id);
+                Assert.True(_registroExiste);
+
+                // select
+                var _registroSelecionado = await _repositorio.SelectAsync(_registroAtualizado.Id);
+                Assert.NotNull(_registroSelecionado);
+                Assert.Equal(_registroAtualizado.Email, _registroSelecionado.Email);
+                Assert.Equal(_registroAtualizado.Name, _registroSelecionado.Name);
+
+                var _todosRegistros = await _repositorio.SelectAsync();
+                Assert.NotNull(_todosRegistros);
+                Assert.True(_todosRegistros.Count() > 1);
+
+                // delete
+                var _removeu = await _repositorio.DeleteAsync(_registroSelecionado.Id);
+                Assert.True(_removeu);
+
+                // Findbylogin
+                var _usuarioPadrao = await _repositorio.FindByLogin("mfrinfo@mail.com");
+                Assert.NotNull(_usuarioPadrao);
+                Assert.Equal("mfrinfo@mail.com", _usuarioPadrao.Email);
+                Assert.Equal("Administrador", _usuarioPadrao.Name);
             }
         }
     }
