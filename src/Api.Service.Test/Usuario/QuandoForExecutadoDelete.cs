339ead5fa06dd92bfadd7158d3455b6a1643c5f2
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-04-08 21:42:51 -0300

    121. Implementar Testes do Método Delete

---
+++ src/Api.Service.Test/Usuario/QuandoForExecutadoDelete.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Interfaces.Services.User;
+using Moq;
+
+namespace Api.Service.Test.Usuario
+{
+    public class QuandoForExecutadoDelete : UsuarioTestes
+    {
+        private IUserService _service;
+        private Mock<IUserService> _serviceMock;
+
+        [Fact(DisplayName = "É possível executar o método Delete.")]
+        public async Task E_Possivel_Executar_Metodo_Delete()
+        {
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Delete(IdUsuario)).ReturnsAsync(true);
+            _service = _serviceMock.Object;
+
+            var deletado = await _service.Delete(IdUsuario);
+            Assert.True(deletado);
+
+            _serviceMock = new Mock<IUserService>();
+            _serviceMock.Setup(m => m.Delete(It.IsAny<long>())).ReturnsAsync(false);
+            _service = _serviceMock.Object;
+
+            deletado = await _service.Delete(0);
+            Assert.False(deletado);
+        }
+    }
+}
