daf452b4a20e739c11ebbdfb61d2d67c2fef7129
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-04-12 22:22:23 -0300

    126. Implementar Teste UsersController - PUT

---
+++ src/Api.Application.Test/Usuario/QuandoRequisitarUpdate/Retorno_BadRequest.cs
@@ -0,0 +1,45 @@
+﻿using Api.Application.Controllers;
+using Domain.Dtos.User;
+using Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarUpdate
+{
+    public class Retorno_BadRequest
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "Não é possível executar o Updated.")]
+        public async Task E_Possivel_Invocar_a_Controller_Update()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Put(It.IsAny<UserDtoUpdate>())).ReturnsAsync(
+                new Domain.Dtos.User.UserDtoUpdateResult
+                {
+                    Id = 1,
+                    Name = nome,
+                    Email = email,
+                    UpdateAt = DateTime.UtcNow,
+                }
+            );
+
+            _controller = new UsersController(serviceMock.Object);
+            _controller.ModelState.AddModelError("Email", "É um campo obrigatório");
+
+            var userDtoUpdate = new UserDtoUpdate
+            {
+                Id = 1,
+                Name = nome,
+                Email = email,
+            };
+
+            var result = await _controller.Put(userDtoUpdate);
+            Assert.True(result is BadRequestObjectResult);
+            Assert.False(_controller.ModelState.IsValid);
+        }
+    }
+}
---
+++ src/Api.Application.Test/Usuario/QuandoRequisitarUpdate/Retorno_Updated.cs
@@ -0,0 +1,48 @@
+﻿using Api.Application.Controllers;
+using Domain.Dtos.User;
+using Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Api.Application.Test.Usuario.QuandoRequisitarUpdate
+{
+    public class Retorno_Updated
+    {
+        private UsersController _controller;
+
+        [Fact(DisplayName = "É possível executar o Updated.")]
+        public async Task E_Possivel_Invocar_a_Controller_Update()
+        {
+            var serviceMock = new Mock<IUserService>();
+            var nome = Faker.Name.FullName();
+            var email = Faker.Internet.Email();
+
+            serviceMock.Setup(m => m.Put(It.IsAny<UserDtoUpdate>())).ReturnsAsync(
+                new Domain.Dtos.User.UserDtoUpdateResult
+                {
+                    Id = 1,
+                    Name = nome,
+                    Email = email,
+                    UpdateAt = DateTime.UtcNow,
+                }
+            );
+
+            _controller = new UsersController(serviceMock.Object);
+
+            var userDtoUpdate = new UserDtoUpdate
+            {
+                Id = 1,
+                Name = nome,
+                Email = email,
+            };
+
+            var result = await _controller.Put(userDtoUpdate);
+            Assert.True(result is OkObjectResult);
+
+            var resultValue = ((OkObjectResult)result).Value as UserDtoUpdateResult;
+            Assert.NotNull(resultValue);
+            Assert.Equal(userDtoUpdate.Name, resultValue.Name);
+            Assert.Equal(userDtoUpdate.Email, resultValue.Email);
+        }
+    }
+}
