9b7e0712d5d7086d38f987063ffbc34e906876ee
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-04-25 01:19:45 -0300

    148. Implementar Testes Unitário de Municipio em Data.Test

---
+++ src/Api.Data.Test/MunicipioCrudCompleto.cs
@@ -0,0 +1,85 @@
+﻿using Data.Context;
+using Data.Implementations;
+using Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Data.Test
+{
+    public class MunicipioCrudCompleto : BaseTest, IClassFixture<DbTeste>
+    {
+        private ServiceProvider _serviceProvider;
+
+        public MunicipioCrudCompleto(DbTeste dbTeste)
+        {
+            _serviceProvider = dbTeste.ServiceProvider;
+        }
+
+        [Fact(DisplayName = "CRUD de Município")]
+        [Trait("CRUD", "MunicipioEntity")]
+        public async Task E_Possivel_Realizar_CRUD_Municipio()
+        {
+            using (var _context = _serviceProvider.GetService<MyContext>())
+            {
+                MunicipioImplementation _repositorio = new MunicipioImplementation(_context);
+                MunicipioEntity _entity = new MunicipioEntity
+                {
+                    Nome = Faker.Address.City(),
+                    CodIBGE = Faker.RandomNumber.Next(1000000, 9999999),
+                    UfId = 25,
+                };
+
+                var _registroCriado = await _repositorio.InsertAsync(_entity);
+                Assert.NotNull(_registroCriado);
+                Assert.Equal(_entity.Nome, _registroCriado.Nome);
+                Assert.Equal(_entity.CodIBGE, _registroCriado.CodIBGE);
+                Assert.Equal(_entity.UfId, _registroCriado.UfId);
+                Assert.False(_registroCriado.Id == 0);
+
+                // Atualizacao
+                _entity.Nome = Faker.Address.City();
+                _entity.Id = _registroCriado.Id;
+                var _registroAtualizado = await _repositorio.UpdateAsync(_entity);
+                Assert.NotNull(_registroAtualizado);
+                Assert.Equal(_entity.Nome, _registroAtualizado.Nome);
+                Assert.Equal(_entity.CodIBGE, _registroAtualizado.CodIBGE);
+                Assert.Equal(_entity.UfId, _registroAtualizado.UfId);
+                Assert.Equal(_registroCriado.Id, _entity.Id);
+
+                var _registroExiste = await _repositorio.ExistAsync(_registroAtualizado.Id);
+                Assert.True(_registroExiste);
+
+                var _registroSelecionado = await _repositorio.SelectAsync(_registroAtualizado.Id);
+                Assert.NotNull(_registroSelecionado);
+                Assert.Equal(_registroAtualizado.Nome, _registroSelecionado.Nome);
+                Assert.Equal(_registroAtualizado.CodIBGE, _registroSelecionado.CodIBGE);
+                Assert.Equal(_registroAtualizado.UfId, _registroSelecionado.UfId);
+                Assert.Null(_registroSelecionado.Uf);
+
+                _registroSelecionado = await _repositorio.GetCompletebyIBGE(_registroAtualizado.CodIBGE);
+                Assert.NotNull(_registroSelecionado);
+                Assert.Equal(_registroAtualizado.Nome, _registroSelecionado.Nome);
+                Assert.Equal(_registroAtualizado.CodIBGE, _registroSelecionado.CodIBGE);
+                Assert.Equal(_registroAtualizado.UfId, _registroSelecionado.UfId);
+                Assert.NotNull(_registroSelecionado.Uf);
+
+                _registroSelecionado = await _repositorio.GetCompletebyId(_registroAtualizado.Id);
+                Assert.NotNull(_registroSelecionado);
+                Assert.Equal(_registroAtualizado.Nome, _registroSelecionado.Nome);
+                Assert.Equal(_registroAtualizado.CodIBGE, _registroSelecionado.CodIBGE);
+                Assert.Equal(_registroAtualizado.UfId, _registroSelecionado.UfId);
+                Assert.NotNull(_registroSelecionado.Uf);
+
+                var _todosRegistros = await _repositorio.SelectAsync();
+                Assert.NotNull(_todosRegistros);
+                Assert.True(_todosRegistros.Count() > 0);
+
+                var _removeu = await _repositorio.DeleteAsync(_registroSelecionado.Id);
+                Assert.True(_removeu);
+
+                _todosRegistros = await _repositorio.SelectAsync();
+                Assert.NotNull(_todosRegistros);
+                Assert.True(_todosRegistros.Count() == 0);
+            }
+        }
+    }
+}
