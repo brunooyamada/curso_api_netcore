6ac8e62e3fe65f8ce7fadd8203d5d1cbdb942af7
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-04-30 00:25:40 -0300

    158. Implementar MunicipiosController

---
+++ src/Api.Application/Controllers/MunicipiosController.cs
@@ -0,0 +1,169 @@
+﻿using Domain.Dtos.Municipio;
+using Domain.Interfaces.Services.Municipio;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Api.Application.Controllers
+{
+    [Authorize("Bearer")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MunicipiosController : ControllerBase
+    {
+        public IMunicipioService _service;
+
+        public MunicipiosController(IMunicipioService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.GetAll());
+            }
+            catch(ArgumentException e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetMunicpioWithId")]
+        public async Task<ActionResult> Get(long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Get(id);
+                if (result == null)
+                {
+                    return NotFound("Registro não encontrado");
+                }
+
+                return Ok(result);
+            }
+            catch(ArgumentException e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{idMunicipio}")]
+        public async Task<ActionResult> GetCompleteById(long idMunicipio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.GetCompleteById(idMunicipio);
+                if (result == null)
+                {
+                    return NotFound("Registro não encontrado");
+                }
+
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("{codigoIBGE}")]
+        public async Task<ActionResult> GetCompleteByIBGE(int codigoIBGE)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.GetCompleteByIBGE(codigoIBGE);
+                if (result == null)
+                {
+                    return NotFound("Registro não encontrado");
+                }
+
+                return Ok(result);
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] MunicipioDtoCreate municipio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Post(municipio);
+                if (result != null)
+                {
+                    return Created(new Uri(Url.Link("GetMunicpioWithId", new { id = result.Id })), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+                
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] MunicipioDtoUpdate municipio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Put(municipio);
+                if (result != null)
+                {
+                    return Created(new Uri(Url.Link("GetMunicpioWithId", new { id = result.Id })), result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
+    }
+}
